520ee4cb1fc4e20fe5d69c785c776d749d5a2303
Danilo <htpps.sw@gmail.com>
2023-12-13 11:35:48 -0300

    Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         <div className="flex flex-col gap-[150px] self-center lg:bg-slate-50 lg:w-[516px] lg:h-[498px] lg:gap-[70px] lg:rounded-2xl">
           <Image src={logo} alt="" width={218} height={33}  className='flex self-center lg:hidden'/>
           <Image src={logorosa} alt="" width={218} height={33}  className='flex self-center  sm:hidden lg:block lg:pt-8'/>
+         
           <form  className='flex flex-col self-center gap-[12px]'>
             <div className='flex flex-col self-center' >
               <label htmlFor="" className='font- text-base text-white  font-thin lg:font-normal lg:text-black'>
@@ -17,6 +18,8 @@ export default function Home() {
               </label>
               <input type="text" className='w-[430px] h-[61px] rounded-lg border border-slate-50 bg-zinc-700 duration-75 hover:rounded-xl lg:bg-white lg:border-slate-200'/>
             </div>
+            
+            
             <div className='flex flex-col self-center' >
               <label htmlFor="" className='font- text-base text-white  font-thin lg:font-normal lg:text-black'>
                 Senha
@@ -24,11 +27,6 @@ export default function Home() {
               <input type="text" className='w-[430px] h-[61px] rounded-lg border border-slate-50 bg-zinc-700 duration-75 hover:rounded-xl lg:bg-white lg:border-slate-200'/>
             </div>
             
-
-           
-
-            
-
             <div className='flex flex-col gap-2 self-center' >
               <button className='w-[430px] h-[61px]  bg-pink-600 text-white font-semibold rounded-lg duration-100 hover:bg-pink-400'>
                 Entrar
@@ -38,7 +36,6 @@ export default function Home() {
                 Esqueci minha senha
               </button>
             </div>
-
           </form>
         </div>
       </main>
